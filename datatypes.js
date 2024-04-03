252fb97a3600dadbeebcdb2c01199f7a1100a5da
Sneha Pandey <148060850+04snehapandey@users.noreply.github.com>
2024-04-03 16:18:47 +0000

    datatypes config done

---
+++ datatypes.js
@@ -0,0 +1,35 @@
+// 7 primitive datatypes 
+
+// NUMBER
+let age = 20 
+console.log(typeof age);
+
+
+// STRING
+let fullName ="sneha pandey";
+console.log(typeof fullName);
+
+// BOOLEAN
+ let isLoggedin = true;
+ console.log(typeof isLoggedin);
+
+// UNDEFINED
+let year;
+console.log(typeof year);
+
+
+// NULL (empty)
+// type of null is object 
+let x = null;
+console.log(typeof x);
+
+
+// BIGINT USED TO STORE LARGE NUMERIC VALUES
+let y = BigInt("123");
+console.log(y)
+console.log(typeof y)
+
+// SYMBOL USED FOR UNIQUE COMPONENTS
+let z = Symbol("hello!")
+console.log(z)
+console.log(typeof z)
