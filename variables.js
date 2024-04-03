3c7a3a4481c9a13c4d37d9d23e8f347868a6eadb
Sneha Pandey <148060850+04snehapandey@users.noreply.github.com>
2024-04-03 03:14:29 +0000

    variables config done

---
+++ variables.js
@@ -0,0 +1,21 @@
+//  variables are containers for data 
+
+
+ //  const is used to define constant variables , cannot be updated or re- declared.
+const acctId = 3456
+let fullName = "sneha pandey"
+
+// var is no longer used .
+var acctEmail = "sneha@google.com"
+acctCity = "kolkata"
+ let acctState;
+
+
+ console.log(acctEmail);
+
+
+  // let can be updated 
+  fullName ="mira pandey"
+
+
+ console.table([acctId,fullName,acctEmail,acctCity,acctState]);
